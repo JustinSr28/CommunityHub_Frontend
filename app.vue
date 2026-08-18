0ed9744ce1d4716d87310410e38b084174e8e097
Sheyla Murillo <murillocruzsheyla36@gmail.com>
2026-08-18 17:11:10 -0600

    Initialize Fronted Project

---
+++ app.vue
@@ -0,0 +1,22 @@
+<script setup>
+const { initAuth } = useAuth()
+
+onMounted(() => {
+  initAuth()
+})
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap');
+
+html,
+body {
+  font-family: 'Inter', sans-serif;
+}
+</style>
---
+++ app/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap');
+
+html,
+body {
+  font-family: 'Inter', sans-serif;
+}
+</style>
